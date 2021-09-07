be12b7654d2fa89e9d569ef7567eb4fa9b74f4ce
bitsbiz <65221311+bitsbiz@users.noreply.github.com>
2021-09-07 23:50:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+from GitHubRepo import GithubRepoCreator
+github_task = GithubRepoCreator(
+    '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', 'https://api.github.com/orgs')
+github_task.create_repo('<your repo goes here>', 'test')
 
